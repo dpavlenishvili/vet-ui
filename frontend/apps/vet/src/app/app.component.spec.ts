c868804ec89e7eb52905ae18812299ccd78fa227
Mikheil <mkvashali@emis.ge>
2023-10-18 03:47:52 +0400

    fix: lint fixes

--- frontend/apps/vet/src/app/app.component.spec.ts
+++ frontend/apps/vet/src/app/app.component.spec.ts
@@ -12,8 +12,6 @@ describe('AppComponent', () => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('h1')?.textContent).toContain(
-            'Welcome vet'
-        );
+        expect(compiled.querySelector('h1')?.textContent).toContain('Welcome vet');
     });
 });
