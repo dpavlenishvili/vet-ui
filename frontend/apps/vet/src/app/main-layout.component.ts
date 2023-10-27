9ed126a2589237bc8ed96e471c67c77a6454be17
Giorgi Cheishvili <giorgi.cheishvili@outlook.com>
2023-10-28 00:03:32 +0400

    feat(frontend): added theming package

--- frontend/apps/vet/src/app/main-layout.component.ts
+++ frontend/apps/vet/src/app/main-layout.component.ts
@@ -1,9 +1,30 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavbarComponent, NavbarLogoDirective } from '@vet/ui/navbar';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
     standalone: true,
-    imports: [],
-    encapsulation: ViewEncapsulation.None,
-    template: ``,
+    imports: [NavbarComponent, RouterOutlet, RouterLink, NavbarLogoDirective],
+    template: `
+        <header>
+            <nav v-ui-navbar>
+                <a routerLink="/" *vUiNavbarLogo>
+                    <img src="assets/evet-logo.svg" alt="Evet Logo" />
+                </a>
+            </nav>
+        </header>
+        <main>
+            <router-outlet></router-outlet>
+        </main>
+    `,
+    styles: [
+        `
+            :host {
+                display: flex;
+                flex-direction: column;
+                flex-grow: 1;
+            }
+        `,
+    ],
 })
 export class MainLayoutComponent {}
