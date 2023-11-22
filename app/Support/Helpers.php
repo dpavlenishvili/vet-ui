406388cc8a36c09ddb763928f51c46df27934f5d
Vaja Sinauridze <vsinauridze@emis.ge>
2023-11-22 15:53:50 +0400

    feat(backend): added forget password api

--- app/Support/Helpers.php
+++ app/Support/Helpers.php
@@ -1,10 +1,19 @@
 <?php
 
-function maskPhoneNumber(string $phoneNumber): string
+function strMask(string $phoneNumber, int $start = null, int $to = null): string
 {
-    $prefix = substr($phoneNumber, 0, 3);
-    $suffix = substr($phoneNumber, -2);
-    $numAsterisks = strlen($phoneNumber) - 5;
+    $prefix = '';
+    $suffix = '';
+
+    if ($start) {
+        $prefix = substr($phoneNumber, 0, $start);
+    }
+
+    if ($to) {
+        $suffix = substr($phoneNumber, $to);
+    }
+
+    $numAsterisks = strlen($phoneNumber) - $start - abs($to);
     $asterisks = str_repeat('*', $numAsterisks);
 
     return $prefix.$asterisks.$suffix;
