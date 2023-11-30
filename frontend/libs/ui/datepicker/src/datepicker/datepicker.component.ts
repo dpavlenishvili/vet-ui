716ee4f88d8118612b610c5b9ae9849adac5c748
Giorgi Cheishvili <giorgi.cheishvili@outlook.com>
2023-11-30 19:13:06 +0400

    feat(frontend): added ngx-bs based datepicker placeholder component

---
+++ frontend/libs/ui/datepicker/src/datepicker/datepicker.component.ts
@@ -0,0 +1,46 @@
+import { Component, inject, Input, ViewEncapsulation } from '@angular/core';
+import { FormControlProvider } from '@vet/ui/form-item';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
+
+let qty = 0;
+
+@Component({
+    selector: 'v-ui-datepicker',
+    templateUrl: './datepicker.component.html',
+    styleUrls: ['./datepicker.component.scss'],
+    standalone: true,
+    providers: [
+        {
+            provide: FormControlProvider,
+            useExisting: DatepickerComponent,
+        },
+    ],
+    encapsulation: ViewEncapsulation.None,
+})
+export class DatepickerComponent extends FormControlProvider implements ControlValueAccessor {
+    @Input() id = `v-ui-datepicker-${++qty}`;
+    ngControl = inject(NgControl, { optional: true });
+
+    constructor() {
+        super();
+        if (this.ngControl) {
+            this.ngControl.valueAccessor = this;
+        }
+    }
+
+    writeValue(obj: any): void {
+        throw new Error('Method not implemented.');
+    }
+
+    registerOnChange(fn: any): void {
+        throw new Error('Method not implemented.');
+    }
+
+    registerOnTouched(fn: any): void {
+        throw new Error('Method not implemented.');
+    }
+
+    setDisabledState?(isDisabled: boolean): void {
+        throw new Error('Method not implemented.');
+    }
+}
