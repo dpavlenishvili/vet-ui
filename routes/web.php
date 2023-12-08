b37f7dc74e90e51bb81d03b5b8b21ca37f58db6c
Giorgi Cheishvili <giorgi.cheishvili@outlook.com>
2023-12-08 17:31:45 +0400

    chore: added catch-all route excluding nova

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('login', function () {
     echo 'login';
 })->name('login');
+
+Route::get('/{any}', function () {
+    return view('welcome');
+})->where('any', '^(?!nova|api).*$');
