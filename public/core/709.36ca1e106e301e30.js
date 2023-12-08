aea890b60d2123f360a4035a12d4006a882d78ad
Giorgi Cheishvili <giorgi.cheishvili@outlook.com>
2023-12-08 17:00:37 +0400

    chore: build deployment

---
+++ public/core/709.36ca1e106e301e30.js
@@ -0,0 +1,19 @@
+'use strict';
+(self.webpackChunkvet = self.webpackChunkvet || []).push([
+    [709],
+    {
+        2709: (e, n, t) => {
+            t.r(n), t.d(n, { default: () => s });
+            const s = [
+                {
+                    path: '',
+                    loadComponent: () =>
+                        t
+                            .e(311)
+                            .then(t.bind(t, 5311))
+                            .then((o) => o.FeaturesRegistrationComponent),
+                },
+            ];
+        },
+    },
+]);
