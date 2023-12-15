e735ea9e4003d1011a4f2f4ffa63370d8c0368d1
Vaja <vaja.sinauridze@stickee.co.uk>
2023-12-15 16:49:16 +0400

    fix(backend): remove Illuminate collection import

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Support\Collection;
 use Spatie\EloquentSortable\Sortable;
 use Spatie\EloquentSortable\SortableTrait;
 
