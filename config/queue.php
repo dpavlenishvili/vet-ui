84c37253ec0fc5dc3c44391657012a587e671717
Vaja <vaja.sinauridze@stickee.co.uk>
2024-03-27 01:46:31 +0400

    fix(backend): update configs

--- config/queue.php
+++ config/queue.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('QUEUE_CONNECTION', 'sync'),
+    'default' => env('QUEUE_CONNECTION', 'rabbitmq'),
 
     /*
     |--------------------------------------------------------------------------
@@ -29,7 +29,19 @@ return [
     */
 
     'connections' => [
+        'rabbitmq' => [
 
+            'driver' => 'rabbitmq',
+            'hosts' => [
+                [
+                    'host' => env('RABBITMQ_HOST', '127.0.0.1'),
+                    'port' => env('RABBITMQ_PORT', 5672),
+                    'user' => env('RABBITMQ_USER', 'guest'),
+                    'password' => env('RABBITMQ_PASSWORD', 'guest'),
+                    'vhost' => env('RABBITMQ_VHOST', '/'),
+                ],
+            ],
+        ],
         'sync' => [
             'driver' => 'sync',
         ],
