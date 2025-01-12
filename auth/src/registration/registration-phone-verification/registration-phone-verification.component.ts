20b5e241de8824044140faf8ec317a1fd5e35603
Giorgi Cheishvili <giorgi.cheishvili@outlook.com>
2025-01-12 20:20:46 +0400

    chore: replaced lodash with lodash-es

--- auth/src/registration/registration-phone-verification/registration-phone-verification.component.ts
+++ auth/src/registration/registration-phone-verification/registration-phone-verification.component.ts
@@ -3,7 +3,7 @@ import { NumericTextBoxComponent } from '@progress/kendo-angular-inputs';
 import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { useAuthEnvironment } from '../../auth.injectors';
 import { RegistrationPhoneTimeoutComponent } from '../registration-phone-timeout/registration-phone-timeout.component';
-import { noop } from 'lodash';
+import noop from 'lodash-es/noop';
 
 @Component({
   selector: 'vet-registration-phone-verification',
