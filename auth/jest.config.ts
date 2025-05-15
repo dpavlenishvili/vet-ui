fb03a39cd5f0b3413e6a04a18c3b75b35e7336b3
Davit Pavlenishvili <davit.pavlenishvili@sap.com>
2025-05-15 19:39:02 +0400

    Delete auth directory

--- auth/jest.config.ts
+++
@@ -1,8 +0,0 @@
-import 'jest-preset-angular';
-
-export default {
-  displayName: 'auth',
-  preset: '../jest.preset.js',
-  coverageDirectory: '../coverage/auth',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-};
